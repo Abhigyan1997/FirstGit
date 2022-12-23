5d820ad6950f9008d8050edd32b8b6893d62074b
Abhigyan1997 <alokabhigyan65@gmail.com>
2022-12-23 11:48:19 +0530

    secondFile

--- myGit.js
+++ myGit.js
@@ -1,1 +1,9 @@
-console,log("This is my first git exprience") 
+//console,log("This is my first git exprience") 
+var a =getElementbyId('header-title');
+a.textContent='Hello'
+var b=getElementbyId('main-header');
+b.style.borderBottom='solid 3px #000'
+//GET ELEMENT  BY CLASS NAME
+var item=document.getElementsByClassName("list-group-item")
+item[2].style.backgroundColor='green';
+item.style.fontWeight='Bold';
