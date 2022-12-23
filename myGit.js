1cddcdd0a8a04ec15187e4b82cd6109fa3ebaaf8
Abhigyan1997 <alokabhigyan65@gmail.com>
2022-12-23 19:14:05 +0530

    ThirdGit

---
+++ myGit.js
@@ -0,0 +1,10 @@
+//get element by class name
+var Items=document.getElementsByClassName('list-group-item');
+Items[5].style.fontWeight='bold';
+Items[5].style.backgroundColor='yellow';
+
+
+//get element by tag name
+ var li=document.getElementsByTagName('li');
+li[5].style.fontWeight='bold';
+li[5].style.backgroundColor='yellow';
