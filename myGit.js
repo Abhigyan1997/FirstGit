e8a491dcd9c355f690aa0008d40632f673d06919
Abhigyan1997 <alokabhigyan65@gmail.com>
2022-12-31 11:05:24 +0530

    EligibleStudent

--- myGit.js
+++ myGit.js
@@ -22,5 +22,17 @@ class student{
  }
  
  student1=new student("Ram",22,78);
- student1.setPlacementAge(11)(40);
  
+ student2=new student("peter",23,1234,79);
+
+student3=new student("sam",25,1234,88);
+
+student4=new student("tim",24,1234,68);
+
+student5=new student('mike',21,1234,43);
+
+student1.setPlacementAge(18)(40);
+student2.setPlacementAge(18)(40);
+student3.setPlacementAge(18)(40);
+student4.setPlacementAge(18)(40);
+student5.setPlacementAge(18)(40);
