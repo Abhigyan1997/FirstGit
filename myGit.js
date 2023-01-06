b88890106cf1a36ac74163ca80e658b9ef09ef14
Abhigyan1997 <121120192+Abhigyan1997@users.noreply.github.com>
2023-01-06 11:30:21 +0530

    Delete myGit.js

--- myGit.js
+++
@@ -1,6 +0,0 @@
-var item=document.getElementsByClassName(".btn");
-var btn=function AddExpense(event){
-  console.log(btn) ;
-  event.preventDefault();
-   console.log(event.target.ExpenseAmount);
-}
